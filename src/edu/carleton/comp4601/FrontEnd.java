67642c9b783a317803024187380d8ba6b99e8d3c
Alex Carlucci <alex.carlucci13@gmail.com>
2018-02-22 21:45:55 -0500

    Make Mock Search route (#2)
    * added mock route for search
    
    * return terms as mock search result

--- src/edu/carleton/comp4601/FrontEnd.java
+++ src/edu/carleton/comp4601/FrontEnd.java
@@ -6,16 +6,33 @@ import javax.ws.rs.core.MediaType;
 @Path("/sda")
 public class FrontEnd{
 
+	
 @GET
 @Produces(MediaType.TEXT_PLAIN)
 public String sayPlainTextHello() {
-	return "Weclome to the SDA";
+	return "Weclome to the SDA3";
 }
 
 @GET
 @Produces(MediaType.TEXT_XML)
 public String sayXmlHello() {
-	return "<?xml version=\"1.0\"?>"+"<sda> Weclome to the SDA"+ "</sda>";
+	return "<?xml version=\"1.0\"?>"+"<sda> Weclome to the SDA2"+ "</sda>";
+}
+
+
+@Path("/search/{terms}")
+@GET
+@Produces(MediaType.TEXT_HTML)
+public String doDistributedSertch(@PathParam("terms") String terms){
+	String[] termsSplice = terms.split("\\+");
+	String termsFormatted = "<h2>Terms</h2>"
+			+ "<ul>";
+			
+	for(int i = 0; i < termsSplice.length; i++){
+		termsFormatted += "<li>" + termsSplice[i] + "</li>";
+	}
+	termsFormatted += "</ul>";
+	return termsFormatted;
 }
 
 }
