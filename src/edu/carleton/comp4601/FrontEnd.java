0aace6c837e7e2f29f0b5eb9d614392286a8cf0b
James McConnell <james.mcconnell@cenx.com>
2018-02-22 19:38:09 -0500

    [AA-00008]renamed things and somehow /rest/bank still exists, commiting to check if web.xml is being pulled from remote

--- src/edu/carleton/comp4601/FrontEnd.java
+++ src/edu/carleton/comp4601/FrontEnd.java
@@ -1,21 +1,21 @@
-package edu.carleton.comp4601.simple;
+package edu.carleton.comp4601;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
-@Path("/bank")
-public class Bank {
+@Path("/sda")
+public class FrontEnd{
 
 @GET
 @Produces(MediaType.TEXT_PLAIN)
 public String sayPlainTextHello() {
-	return "Weclome to the bank Plain text";
+	return "Weclome to the SDA";
 }
 
 @GET
 @Produces(MediaType.TEXT_XML)
 public String sayXmlHello() {
-	return "<?xml version=\"1.0\"?>"+"<bank> Weclome to the bank XML"+ "</bank>";
+	return "<?xml version=\"1.0\"?>"+"<sda> Weclome to the SDA"+ "</sda>";
 }
 
 }
